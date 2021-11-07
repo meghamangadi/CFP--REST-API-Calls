6fcdc6c73e4ccf29c962af368f339adf3ab550e5
megha angadi <meghamangadi@gmail.com>
2021-11-07 21:02:41 +0530

    Added :Rest call to show Hello with firstname and lastname using Post request

--- src/main/java/com/bridgelabz/demoprogram/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/demoprogram/controller/HelloRestController.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.demoprogram.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.demoprogram.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -20,9 +24,14 @@ public class HelloRestController {
 	public String sayHello(@RequestParam(value = "name") String name) {
 		return "Hello " + name + " From Bridgelabz";
 	}
-	
+
 	@RequestMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
-	    return "Hello " + name + " From Bridgelabz";
+		return "Hello " + name + " From Bridgelabz";
+	}
+
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + " From Bridgelabz";
 	}
 }
