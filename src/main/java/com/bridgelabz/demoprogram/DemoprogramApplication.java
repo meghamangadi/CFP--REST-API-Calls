13ed37912d2cd9372b769b018ef1fa7f03d35a2e
megha angadi <meghamangadi@gmail.com>
2021-11-07 18:05:45 +0530

    Added: Created a Rest Controller to demonstrate the various HTTP Methods and respond hello messages to the user

--- src/main/java/com/bridgelabz/demoprogram/DemoprogramApplication.java
+++ src/main/java/com/bridgelabz/demoprogram/DemoprogramApplication.java
@@ -8,6 +8,7 @@ public class DemoprogramApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoprogramApplication.class, args);
+		
 	}
 
 }
--- src/main/java/com/bridgelabz/demoprogram/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/demoprogram/controller/HelloRestController.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.demoprogram.controller;
 
-public class HelloRestController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+	
+	@RequestMapping(value={"","/","/home"})
+	public String sayHello() {
+		return "Hello From BridgeLabz" ;
+		
+	}
 }
